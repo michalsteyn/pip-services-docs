2ae9e2b6bcec3221d94096728cdb00b3f202e0e8
banalna <banalna@users.noreply.github.com>
2021-10-14 23:10:39 +0000

    deploy: 6f8aa9955aa59fed8acfd73ddbb97b668b3c7354

--- js/content-tab-selector.js
+++ js/content-tab-selector.js
@@ -1,26 +1,28 @@
-// Set events
-let buttonGroups = document.getElementsByClassName('tab-selector-btn-group')
 
-if (buttonGroups.length > 0) {
-    let buttons = Array.from(buttonGroups[0].getElementsByTagName('button'));
+// Set events
+let tabSelectors = Array.from(document.getElementsByClassName("content-tab-selector"));
+
+tabSelectors.forEach(tabSelector => {
+    let buttons = Array.from(tabSelector.getElementsByClassName('tab-selector-btn-group')[0].getElementsByTagName('button'));
 
     buttons.forEach(button => {
         button.addEventListener('click', showSelected);
     })
 
+    
     showFirstWithContent(buttons);
-}
+});
 
 // by default show first with content
-function showFirstWithContent(buttons) {
+function showFirstWithContent(buttons){
     const reservedKeywords = ['not available'];
     let contentSections = Array.from(buttons[0].parentElement.parentElement.getElementsByClassName('content-tab-section'));
 
-    for (let index = 0; index < contentSections.length; index++) {
+    for (let index = 0; index < contentSections.length; index++){
         let value = contentSections[index];
-        if (value.innerText != undefined
-            && value.innerText != null
-            && value.innerText.trim() != ''
+        if (value.innerText != undefined 
+            && value.innerText != null 
+            && value.innerText.trim() != '' 
             && !reservedKeywords.includes(value.innerText.trim().toLowerCase())) {
 
             showSelected({ target: buttons[index] });
@@ -28,34 +30,30 @@ function showFirstWithContent(buttons) {
         }
     }
 }
-// show selected section
+
+
 function showSelected(e) {
-    let contentSections = Array.from(document.getElementsByClassName('content-tab-section'));
+    let contentSections = Array.from(e.target.parentElement.parentElement.getElementsByClassName('content-tab-section'));
     let buttons = Array.from(e.target.parentElement.getElementsByTagName('button'));
     let selectedIndex = buttons.indexOf(e.target);
 
-    if (contentSections.length != buttons.length && contentSections.length % buttons.length != 0)
-        throw Error('The number of buttons must be equal to or a multiple of the number of sections. Buttons: ' +
-            contentSections.length +
+    if (contentSections.length != buttons.length)
+        throw Error('The number of buttons should be equal to the number of sections. Buttons: ' +
+            contentSections.length + 
             ', Sections: ' + contentSections.length);
-
+    
 
     // Show selected
     e.target.classList.add('active');
     e.target.removeAttribute('hidden');
-
-    contentSections.forEach((section) => {
-        section.setAttribute('hidden', "");
-    });
-
-    for (let index = selectedIndex; index < contentSections.length - 1; index += buttons.length) {
-        contentSections[index].removeAttribute('hidden');
-    }
+    contentSections[selectedIndex].removeAttribute('hidden');
 
     // Hide other
     buttons.forEach((button, index) => {
         if (selectedIndex != index) {
             button.classList.remove('active');
+            contentSections[index].setAttribute('hidden', "");
         }
+        
     });
 }
